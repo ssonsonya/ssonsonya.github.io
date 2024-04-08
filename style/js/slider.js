9d067505adb6249aa41fbc0a7897a254a7aa2238
ssonsonya <hyonseonkim@gmail.com>
2024-04-08 23:52:47 +0900

    [ Style ] Revised display style

--- style/js/slider.js
+++ style/js/slider.js
@@ -11,12 +11,23 @@ function runSlider(event)
     .target.closest('.slider_thumb')
     .dataset.thumbId;
 
-    // Remove active class (thumbs)
+    // Remove active class
     sliderThumbs
     .querySelector('.slider_thumb_active').classList.remove('slider_thumb_active');
+    sliderImgs
+    .querySelector('.slider_img_active').classList.remove('slider_img_active');
+    sliderTitles
+    .querySelector('.slider_info_active').classList.remove('slider_info_active');
+    sliderDescs
+    .querySelector('.slider_info_active').classList.remove('slider_info_active');
       
-    // Add active class (thumbs)
+    // Add active class
     sliderThumbs
-    .querySelector(`[data-thumb-id='${index}']`)
-    .classList.add('slider_thumb_active');
+    .querySelector(`[data-thumb-id='${index}']`).classList.add('slider_thumb_active');
+    sliderImgs
+    .querySelector(`[data-img-id='${index}']`).classList.add('slider_img_active');
+    sliderTitles
+    .querySelector(`[data-title-id='${index}']`).classList.add('slider_info_active');
+    sliderDescs
+    .querySelector(`[data-desc-id='${index}']`).classList.add('slider_info_active');
 }
